da133a3e6148a8a48c0fdccace7f2347df8da8d0
azawslearn <134204024+azawslearn@users.noreply.github.com>
2024-01-23 12:48:21 +0200

    Update web_app.py

--- web/web_app.py
+++ web/web_app.py
@@ -39,4 +39,4 @@ def delete_item(id):
     return redirect('/items')
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000,host='0.0.0.0')
+    app.run(debug=True, port=80,host='0.0.0.0')
